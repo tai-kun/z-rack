dc72326e3a6598ac3bdeccc8612ad160849b02ce
tai-kun <taikun.dev@gmail.com>
2026-06-06 17:15:35 +0900

    test: chromium のみ

--- z-rack/.config/vitest.client.ts
+++ z-rack/.config/vitest.client.ts
@@ -19,7 +19,11 @@ export default defineConfig({
       provider: playwright(),
       enabled: true,
       headless: true,
-      instances: [{ browser: "chromium" }, { browser: "firefox" }, { browser: "webkit" }],
+      instances: [
+        { browser: "chromium" },
+        // { browser: "firefox" },
+        // { browser: "webkit" },
+      ],
     },
     setupFiles: [".config/_debugging.ts"],
   },
